bb09e50e6f530674e18a6c9649f82732659f0b46
davide.patti <davide.patti@autotrader.co.uk>
2022-05-22 18:20:37 +0100

    re enable auth

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Homepage from "./Screen/Homepage";
 import Password from "./Screen/Password";
 
 export default function App() {
-    const [isAuth, setIsAuth] = useState(true);
+    const [isAuth, setIsAuth] = useState(false);
 
       return (
           <Router>
